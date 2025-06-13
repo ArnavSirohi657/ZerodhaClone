5bf3dcded00aa2e2bd87bab2d4e3a315840c7028
Arnav Sirohi <arnavsirohi007@gmail.com>
2025-06-13 21:54:07 +0530

    final commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 // Importing components from their respective folders
 import HomePage from './HOME/HomePage';
-import SignUp from './SignUp/SignUp';
+import SignIn from './SignIn/SignIn.jsx';
 import About from './about/AboutPage';  // Changed from LandingPage/About to about/AboutPage
 import { ProductPage } from './landing_page/products/ProductsPage.js';  // Assuming this is where Product.js is
 import PricingPage from './Pricing/PricingPage.jsx';  // Assuming this is where Support.js is
@@ -16,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     <NavBar />
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup" element={<SignIn />} />
       <Route path="/about" element={<About />} />
       <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
