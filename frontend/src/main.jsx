0d625cedcd72557abedfafff7e6e8d0ac058ae62
Arnav Sirohi <arnavsirohi007@gmail.com>
2025-06-13 22:11:01 +0530

    final commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Importing components from their respective folders
 import HomePage from './HOME/HomePage';
 import SignIn from './SignIn/SignIn.jsx';  
-import AboutPage from "About/AboutPage" // Changed from LandingPage/About to about/AboutPage
+import AboutPage from "./About/AboutPage" // Changed from LandingPage/About to about/AboutPage
 import { ProductPage } from './Products/ProductPage.jsx';  // Assuming this is where Product.js is
 import PricingPage from './Pricing/PricingPage.jsx';  // Assuming this is where Support.js is
 import NavBar from './NavBar.jsx';                  // Changed from NavBar to Navbar.js
